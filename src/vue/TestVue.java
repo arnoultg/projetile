639e8c4bb242bde8b05bf61b9d6e3d530053b429
Arnoult Gregoire <gregoire.arnoult@free.fr>
2018-05-29 16:43:09 +0200

    initial commit

--- src/vue/TestVue.java
+++ src/vue/TestVue.java
@@ -8,6 +8,7 @@ package vue;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,55 +18,16 @@ import javax.swing.JPanel;
  * @author lavierp
  */
 public class TestVue {
-
-    private JFrame frame;
-    private JButton bouton;
-
-    /**
-     * @param args the command line arguments
-     */
-    public TestVue() {
-        frame = new JFrame();
-        frame.setTitle("Ma carte");
-        frame.setSize(800, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        frame.add(mainPanel);
-        JPanel panelCentre = new JPanel(new GridLayout(6, 6));
-
-        bouton = new JButton();
-        bouton.setBackground(Color.red);
-        mainPanel.add(panelCentre, BorderLayout.CENTER);
-        frame.setVisible(true);
-
-        for (int i = 1; i <= 36; i++) {
-            panelCentre.add(getCellule(i));
-
-        }
-
-    }
-
-    private JPanel getCellule(int i) {
-        int numLigne = (int) (i + 4) / 4;
-        int numCouleur = (i - numLigne + 1) % 4 + 1;
-        JPanel panelCellule = new JPanel();
-        JButton tesb = new JButton();
-        tesb.setBackground(Color.red);
-        panelCellule.add(tesb);
-        //panelCellule.setBackground((numCouleur == 1 ? new Color(255, 106, 0) : (numCouleur == 2 || numCouleur == 4 ? new Color(173, 234, 80) : new Color(92, 147, 200))));
-        return panelCellule;
-    }
-
-    public void afficher() {
-
-    }
-
     public static void main(String[] args) {
         // TODO code application logic here
-        TestVue jeu = new TestVue();
+        vuejeu jeu = new vuejeu();
         jeu.afficher();
 
     }
 
 }
+
+ 
+
+
+  
--- src/vue/vuejeu.java
+++ src/vue/vuejeu.java
@@ -5,29 +5,81 @@
  */
 package vue;
 
+import Enums.Iles;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import javax.swing.JPanel;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 /**
  *
  * @author lavierp
  */
 public class vuejeu extends JPanel implements Observe {
+
+    private JFrame frame;
+    private ArrayList<JButton> lesbouttons;
+    private Observateur observateur;
     
- private Observateur observateur;    
+
+    public vuejeu() {
+        frame = new JFrame();
+        frame.setTitle("Carte");
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        frame.add(mainPanel);
+        JPanel panelCentre = new JPanel(new GridLayout(6, 6));
+        lesbouttons = new ArrayList<>();
+        JButton bouton = new JButton();
+        int compteur = 0;
+
+        mainPanel.add(panelCentre, BorderLayout.CENTER);
+        for (int i = 1; i <= 36; i++) {
+            if ((i < 3) || (i > 4 && i < 8) || (i == 12) || (i == 25) || (i > 29 && i < 33) || (i > 34)) {
+                JPanel blanc = new JPanel();
+                panelCentre.add(blanc);
+            } else {
+                panelCentre.add(getCellule(compteur));
+                compteur ++;
+                System.out.println(compteur);
+                
+
+            }
+
+        }
+
+    }
+
+    private JButton getCellule(int compteur) {
+
+        JButton bouton = new JButton(Iles.values()[compteur].toString());
+        lesbouttons.add(bouton);
+
+        return bouton;
+    }
+
+    public void afficher() {
+        frame.setVisible(
+                true);
+
+    }
+
     public void addObservateur(Observateur o) {
         this.observateur = o;
     }
-    
+
     public void notifierObservateur(Message m) {
         if (observateur != null) {
             observateur.traiterMessage(m);
         }
-    }  
-    
+    }
 
 }
